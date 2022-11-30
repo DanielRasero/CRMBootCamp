80b31872291ab2f02aeed9d5135580e981df4d3a
danielRasero <danielraseromairn@gmail.com>
2022-11-30 10:42:12 +0100

    Change date to localdate in ContactConverter

--- src/main/java/com/CRM/weekAssessment/converters/ContactConverter.java
+++ src/main/java/com/CRM/weekAssessment/converters/ContactConverter.java
@@ -7,7 +7,7 @@ import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
-public class contactConverter {
+public class ContactConverter {
 
     public static ContactDTO ToDTO(Contact contact) {
 
